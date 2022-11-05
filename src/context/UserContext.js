137d5e64a83516fd5d666fe0937197d0587e1e71
Mahinkahn <mahinkahn37@gmail.com>
2022-11-05 11:43:42 +0600

    AuthContext

---
+++ src/context/UserContext.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createContext } from 'react';
+
+export const AuthContext = createContext();
+
+const UserContext = ({ children }) => {
+    return (
+        <AuthContext.Provider>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default UserContext;
